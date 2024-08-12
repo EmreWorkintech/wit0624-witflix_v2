d5ece7e02b7a217948dd020002b7ec964d51786d
Emre Şahiner <emre@workintech.com.tr>
2024-08-12 12:28:58 +0300

    routing added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
+import {
+  Redirect,
+  Route,
+  Switch,
+} from "react-router-dom/cjs/react-router-dom.min";
+import Login from "./pages/Login";
+import Welcome from "./pages/Welcome";
+import Browse from "./pages/Browse";
+
 function App() {
   return (
     <>
-      <p>Emre</p>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/login" />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/welcome">
+          <Welcome />
+        </Route>
+        <Route path="/browse">
+          <Browse />
+        </Route>
+      </Switch>
     </>
   );
 }
